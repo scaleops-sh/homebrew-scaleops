15b578a7d465e9be9bed6e2318e0a0554e2817d8
Vladislav Shub <1196390+vladshub@users.noreply.github.com>
2022-06-07 08:12:07 +0300

    better ruby code

--- HomebrewFormula/lib/custom_download_strategy.rb
+++ HomebrewFormula/lib/custom_download_strategy.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "download_strategy"
 
 # GitHubPrivateRepositoryDownloadStrategy downloads contents from GitHub
@@ -18,9 +20,8 @@ class GitHubPrivateRepositoryDownloadStrategy < CurlDownloadStrategy
   end
 
   def parse_url_pattern
-    unless match = url.match(%r{https://github.com/([^/]+)/([^/]+)/(\S+)})
-      raise CurlDownloadStrategyError, "Invalid url pattern for Scaleops."
-    end
+    match = url.match(%r{https://github.com/([^/]+)/([^/]+)/(\S+)})
+    raise CurlDownloadStrategyError, "Invalid url pattern for Scaleops." unless match
 
     _, @owner, @repo, @filepath = *match
   end
@@ -37,9 +38,7 @@ class GitHubPrivateRepositoryDownloadStrategy < CurlDownloadStrategy
 
   def set_github_token
     @github_token = ENV.fetch("HOMEBREW_SCALEOPS_TOKEN")
-    unless @github_token
-      raise CurlDownloadStrategyError, "Environmental variable HOMEBREW_SCALEOPS_TOKEN is required."
-    end
+    raise CurlDownloadStrategyError, "Environmental variable HOMEBREW_SCALEOPS_TOKEN is required." unless @github_token
   end
 end
 
@@ -48,15 +47,10 @@ end
 # of your formula. This download strategy uses GitHub access tokens (in the
 # environment variables SCALEOPS_TOKEN) to sign the request.
 class GitHubPrivateRepositoryReleaseDownloadStrategy < GitHubPrivateRepositoryDownloadStrategy
-  def initialize(url, name, version, **meta)
-    super
-  end
 
   def parse_url_pattern
     url_pattern = %r{https://github.com/([^/]+)/([^/]+)/releases/download/([^/]+)/(\S+)}
-    unless @url =~ url_pattern
-      raise CurlDownloadStrategyError, "Invalid url pattern for Scaleops Release."
-    end
+    raise CurlDownloadStrategyError, "Invalid url pattern for Scaleops Release." unless url_pattern.match(@url)
 
     _, @owner, @repo, @tag, @filename = *@url.match(url_pattern)
   end
@@ -70,7 +64,10 @@ class GitHubPrivateRepositoryReleaseDownloadStrategy < GitHubPrivateRepositoryDo
   def _fetch(url:, resolved_url:, timeout:)
     # HTTP request header `Accept: application/octet-stream` is required.
     # Without this, the GitHub API will respond with metadata, not binary.
-    curl_download download_url, "--header", "Accept: application/octet-stream", "--header", "Authorization: token #{@github_token}", to: temporary_path
+    args = [
+      download_url, "--header", "Accept: application/octet-stream", "--header", "Authorization: token #{@github_token}"
+    ]
+    curl_download *args, to: temporary_path
   end
 
   def asset_id
@@ -81,6 +78,7 @@ class GitHubPrivateRepositoryReleaseDownloadStrategy < GitHubPrivateRepositoryDo
     release_metadata = fetch_release_metadata
     assets = release_metadata["assets"].select { |a| a["name"] == @filename }
     raise CurlDownloadStrategyError, "Asset file not found." if assets.empty?
+
     assets.first["id"]
   end
 
