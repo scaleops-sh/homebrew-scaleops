8429426d47e4dfab29d7710f48853eac30bad67a
Brew Updater <actions@users.noreply.github.com>
2022-06-29 18:21:26 +0300

    Brew formula update for scaleops version v0.0.25

--- HomebrewFormula/scaleops.rb
+++ HomebrewFormula/scaleops.rb
@@ -10,7 +10,7 @@ class Scaleops < Formula
 
   on_macos do
     url "https://github.com/scaleops-sh/scaleops-sh/releases/download/v0.0.25/scaleops_0.0.25_darwin_all.tar.gz"
-    sha256 "88becc18131c87fcaf4f3b41cac2724dc9408d8369ca3b153c03cc67701988e4"
+    sha256 "8f0c8df2fbd0e504251e09f90c7827754d1f214e3e89259a9aa62d2cbe358c97"
 
     def install
       bin.install "scaleops"
@@ -23,7 +23,7 @@ class Scaleops < Formula
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/scaleops-sh/scaleops-sh/releases/download/v0.0.25/scaleops_0.0.25_linux_arm64.tar.gz"
-      sha256 "2154cb1fa68ca45c0f184e6bcda36f7972af8a0f595a13fcd7ddaa8f33c66677"
+      sha256 "81c9025d8192b504d25eb5d52f5d0406563cfa62db4b89db221e9a073b3a347f"
 
       def install
         bin.install "scaleops"
@@ -34,7 +34,7 @@ class Scaleops < Formula
     end
     if Hardware::CPU.intel?
       url "https://github.com/scaleops-sh/scaleops-sh/releases/download/v0.0.25/scaleops_0.0.25_linux_amd64.tar.gz"
-      sha256 "7d1a13e3353aaae1946a38a0c952841bc061b3c72a6c1afe4a5188c138f5b12c"
+      sha256 "319874bb7c5c3035753ff3cee1d7e5bdb03d6cbb7f52262694bce99b1a475a71"
 
       def install
         bin.install "scaleops"
