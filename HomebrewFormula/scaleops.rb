ef1baaead2cf815542df9febc47d3e2689ca70e2
Brew Updater <actions@users.noreply.github.com>
2022-06-29 16:48:59 +0300

    Brew formula update for scaleops version v0.0.24

--- HomebrewFormula/scaleops.rb
+++ HomebrewFormula/scaleops.rb
@@ -5,44 +5,56 @@
 class Scaleops < Formula
   desc "CLI"
   homepage "https://scaleops.sh/"
-  version "0.0.23"
+  version "0.0.24"
   license "Private"
 
   on_macos do
-    url "https://github.com/scaleops-sh/scaleops-sh/releases/download/v0.0.23/scaleops_0.0.23_darwin_all.tar.gz"
-    sha256 "0d7b00117a4c5ecac47ef967f5d99285f8ba4d4cfd8b8a75fc03855cedbd25cd"
+    url "https://github.com/scaleops-sh/scaleops-sh/releases/download/v0.0.24/scaleops_0.0.24_darwin_all.tar.gz"
+    sha256 "eca8a16575150aab9332942105e6ef4823e91e04ec909786018a6d35566b759c"
 
     def install
       bin.install "scaleops"
+      (bash_completion/"scaleops").write `#{bin}/scaleops completion -s bash`
+      (fish_completion/"scaleops.fish").write `#{bin}/scaleops completion -s fish`
+      (zsh_completion/"_scaleops").write `#{bin}/scaleops completion -s zsh`
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/scaleops-sh/scaleops-sh/releases/download/v0.0.23/scaleops_0.0.23_linux_arm64.tar.gz"
-      sha256 "d9c153bbffaf8cfa51df39eb5d9c2e4f4dd3c9afccf5a36377bc48c4b9935d53"
+      url "https://github.com/scaleops-sh/scaleops-sh/releases/download/v0.0.24/scaleops_0.0.24_linux_arm64.tar.gz"
+      sha256 "f4301cf50866ac7cf76f5d12b4144aedc3c5161c72e45f8b588ba03933f4bb89"
 
       def install
         bin.install "scaleops"
+        (bash_completion/"scaleops").write `#{bin}/scaleops completion -s bash`
+        (fish_completion/"scaleops.fish").write `#{bin}/scaleops completion -s fish`
+        (zsh_completion/"_scaleops").write `#{bin}/scaleops completion -s zsh`
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/scaleops-sh/scaleops-sh/releases/download/v0.0.23/scaleops_0.0.23_linux_amd64.tar.gz"
-      sha256 "53699d1aae6aaacab812019f985dfe2d794dc83be4c84024c5743c126b46f43a"
+      url "https://github.com/scaleops-sh/scaleops-sh/releases/download/v0.0.24/scaleops_0.0.24_linux_amd64.tar.gz"
+      sha256 "cee2f4628ebf6245c456892f75af5fa963732b72bd02f21b7af8e8d31d6b61bb"
 
       def install
         bin.install "scaleops"
+        (bash_completion/"scaleops").write `#{bin}/scaleops completion -s bash`
+        (fish_completion/"scaleops.fish").write `#{bin}/scaleops completion -s fish`
+        (zsh_completion/"_scaleops").write `#{bin}/scaleops completion -s zsh`
       end
     end
   end
 
   depends_on "helm"
   depends_on "kubectl"
+  depends_on "zsh" => :optional
 
   def caveats; <<~EOS
     Now get the token from scaleops.sh
     Then run:
       scaleops system install --token *****
+
+    Note: To enjoy autocompletion please make sure to setup brew correctly https://docs.brew.sh/Shell-Completion
   EOS
   end
 end
